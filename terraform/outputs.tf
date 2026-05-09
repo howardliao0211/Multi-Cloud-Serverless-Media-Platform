a2e3aab27ef5c2b5b9c00acd938b606cde8ae65c
howardliao0211 <howard.liao.hh@gmail.com>
2026-05-10 00:32:23 +1000

    feat: added terraform files for provisioning in AWS

---
+++ terraform/outputs.tf
@@ -0,0 +1,19 @@
+output "aws_region" {
+  value = var.aws_region
+}
+
+output "cognito_user_pool_id" {
+  value = aws_cognito_user_pool.main.id
+}
+
+output "cognito_user_pool_client_id" {
+  value = aws_cognito_user_pool_client.frontend.id
+}
+
+output "cognito_domain" {
+  value = "${aws_cognito_user_pool_domain.main.domain}.auth.${var.aws_region}.amazoncognito.com"
+}
+
+output "hosted_ui_login_url" {
+  value = "https://${aws_cognito_user_pool_domain.main.domain}.auth.${var.aws_region}.amazoncognito.com/login?client_id=${aws_cognito_user_pool_client.frontend.id}&response_type=code&scope=openid+email+profile&redirect_uri=http://localhost:5173"
+}
