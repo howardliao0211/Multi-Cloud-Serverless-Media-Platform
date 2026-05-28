e3ae7d0c1c0d7322b4e52f39a71631c0a159c229
Chung-Yu Liu <auzhwa@users.noreply.github.com>
2026-05-28 22:02:29 +1000

    infra: add Firestore metadata replica database

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -45,3 +45,15 @@ VITE_COGNITO_USER_POOL_CLIENT_ID="${var.existing_cognito_user_pool_client_id}"
 VITE_API_URL="${data.aws_apigatewayv2_api.main.api_endpoint}"
 EOT
 }
+
+output "gcp_firestore_database_name" {
+  value = google_firestore_database.metadata_replica.name
+}
+
+output "gcp_firestore_location" {
+  value = google_firestore_database.metadata_replica.location_id
+}
+
+output "gcp_firestore_type" {
+  value = google_firestore_database.metadata_replica.type
+}
