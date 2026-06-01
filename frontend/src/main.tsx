f20899d4b3d4193ae40cdd8cdce9a11ecfde8c12
Meng-Chiu Huang <sandy.mchuang@gmail.com>
2026-06-01 15:30:30 +1000

    feat: add react-router-dom for routing and wrap App component in BrowserRouter

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom';
 import './index.css'
 import "./amplify-config";
 import App from "./App.tsx";
@@ -12,6 +13,8 @@ if (!rootElement) {
 
 createRoot(rootElement).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StrictMode>,
 );
