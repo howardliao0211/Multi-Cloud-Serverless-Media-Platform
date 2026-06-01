929fccb52f3364db19d877c5a104f7a7fa78d58d
Meng-Chiu Huang <sandy.mchuang@gmail.com>
2026-06-01 15:44:51 +1000

    feat: add ProtectedRoute component for authentication handling and redirecting to login

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,47 @@
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { Navigate } from "react-router-dom";
+import { getCurrentUser } from "aws-amplify/auth";
 import LoginScreen from "./Screens/LoginScreen.tsx"
 import RegisterScreen from "./Screens/RegisterScreen.tsx";
 import DashboardScreen from "./Screens/DashboardScreen.tsx";
 import koala from "./assets/koala.png";
 
+function ProtectedRoute({ children }: { children: React.ReactNode}) {
+  const [isAuthenticated, setIsAuthenticated] = useState<boolean | null>(null);
+  const [checkingAuth, setCheckingAuth] = useState<boolean>(true);
+
+  useEffect(() => {
+    async function checkAuth() {
+      try {
+        await getCurrentUser();
+        setIsAuthenticated(true);
+      } catch (error) {
+        setIsAuthenticated(false);
+      } finally {
+        setCheckingAuth(false);
+      }
+    }
+  }, []);
+
+  if (checkingAuth) {
+    return <main className="app-container">
+      <p>Loading...</p>
+    </main>;
+  }
+
+  if (!isAuthenticated) {
+    return <Navigate to="/login" replace />;
+  }
+
+  return children;
+}
+
+
 function App(){
   const [view, setView] = useState<string>("Welcome");
   const [showRegisterSuccess, setShowRegisterSuccess] = useState<boolean>(false);
+
 
   function handleSuccess(){
     console.log("login successfull");
