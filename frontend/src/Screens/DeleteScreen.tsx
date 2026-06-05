140bce993222d3d7bbf91052daa652a9d1586f5f
Meng-Chiu Huang <sandy.mchuang@gmail.com>
2026-06-05 17:42:34 +1000

    feat: implement delete functionality in DeleteScreen; enhance state management and error handling

--- frontend/src/Screens/DeleteScreen.tsx
+++ frontend/src/Screens/DeleteScreen.tsx
@@ -1,8 +1,31 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useOutletContext } from "react-router-dom";
+import { authFetch } from "../services/api";
+import { getErrorMessage, type EditTagsResponse } from "../utils";
 
-function DeleteScreen(){
+type DashboardOutletContext = {
+    selectedUrls: string[];
+    setSelectedUrls: React.Dispatch<React.SetStateAction<string[]>>;
+}
+
+function DeleteScreen() {
     const [deleteUrlText, setDeleteUrlText] = useState<string>("");
-    return(
+
+    const [isLoading, setIsLoading] = useState<boolean>(false);
+    const [error, setError] = useState<string | null>(null);
+    const [successMessage, setSuccessMessage] = useState<string | null>(null);
+
+    const { selectedUrls, setSelectedUrls, } = useOutletContext<DashboardOutletContext>();
+
+    useEffect(() => {
+        setDeleteUrlText(selectedUrls.join("\n"));
+    }, [selectedUrls]);
+
+    async function handleDelete(){
+
+    }
+    
+    return (
         <main className="dashboard-content">
             <h1>Manage Files</h1>
 
@@ -10,8 +33,34 @@ function DeleteScreen(){
                 <h2>⚠️ Delete Files</h2>
                 <p>Files, thumbnails, and all database entries will be permanently removed.</p>
                 <p>File URLs to delete (one per line)</p>
-                <textarea value={deleteUrlText} onChange={(event) => setDeleteUrlText(event.target.value)} placeholder="https://s3.amazonaws.com/bucket/file.jpg" />
-                <button className="button-row" type="button">Delete Files</button>
+                <textarea value={deleteUrlText} onChange={(event) => {
+                    const value = event.target.value;
+
+                    setDeleteUrlText(value);
+
+                    setSelectedUrls(
+                        Array.from(
+                            new Set(value.split("\n").map((url) => url.trim()).filter(Boolean))
+                        )
+                    );
+                }} placeholder="https://s3.amazonaws.com/bucket/file.jpg" />
+
+                <button className="button-row" type="button" onClick={handleDelete}>Delete Files</button>
+
+                {successMessage && (
+                    <p className="success-message">
+                        {successMessage}
+                    </p>
+                )}
+
+                {error && (
+                    <p
+                        className="error-message"
+                        style={{ whiteSpace: "pre-wrap" }}
+                    >
+                        {error}
+                    </p>
+                )}
             </section>
         </main>
     );
