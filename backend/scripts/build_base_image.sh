058e615ce65fad04be02308e4b085d05e634bc5c
Howard Liao <hlia0034@student.monash.edu>
2026-06-08 19:31:32 +1000

    Squashed commit of the following:
    commit a20c6d72fa0f0db951df64c39aeb6beea4e8f3e8
    Author: Pi-Chieh <nike8727@gmail.com>
    Date:   Mon Jun 8 19:12:45 2026 +1000
    
        fix: update CD pipeline
    
    commit e395df540759c37d5ceb4b21994429689195a831
    Author: Howard Liao <hlia0034@student.monash.edu>
    Date:   Mon Jun 8 17:17:15 2026 +1000
    
        dispatching CI/CD
    
    commit ed189232984e10aa779d2d96acc910d3f401e4a1
    Author: Howard Liao <hlia0034@student.monash.edu>
    Date:   Mon Jun 8 17:15:59 2026 +1000
    
        working on deployment script

--- backend/scripts/build_base_image.sh
+++ backend/scripts/build_base_image.sh
@@ -6,6 +6,20 @@ BASE_IMAGE_DIR="${PROJECT_ROOT}/base_images"
 BASE_IMAGE_NAME="${BASE_IMAGE_NAME:-ml_base}"
 DOCKERFILE="${DOCKERFILE:-${BASE_IMAGE_DIR}/${BASE_IMAGE_NAME}/Dockerfile}"
 BUILD_CONTEXT="${BUILD_CONTEXT:-${BASE_IMAGE_DIR}/${BASE_IMAGE_NAME}}"
+ARCHITECTURE="${ARCHITECTURE:-${AWS_ARCHITECTURE:-x86_64}}"
+
+case "${ARCHITECTURE}" in
+  x86_64)
+    DOCKER_PLATFORM="linux/amd64"
+    ;;
+  arm64)
+    DOCKER_PLATFORM="linux/arm64"
+    ;;
+  *)
+    echo "Error: unsupported ARCHITECTURE '${ARCHITECTURE}'. Use x86_64 or arm64." >&2
+    exit 1
+    ;;
+esac
 
 if [[ ! -f "${DOCKERFILE}" ]]; then
     echo "Error: Dockerfile does not exist: ${DOCKERFILE}" >&2
@@ -15,8 +29,10 @@ fi
 echo "Building base image: ${BASE_IMAGE_NAME}"
 echo "Dockerfile: ${DOCKERFILE}"
 echo "Build context: ${BUILD_CONTEXT}"
+echo "Platform: ${DOCKER_PLATFORM}"
 
-docker buildx build \
+docker build \
+    --platform "${DOCKER_PLATFORM}" \
     -t "${BASE_IMAGE_NAME}" \
     -f "${DOCKERFILE}" \
     "${BUILD_CONTEXT}"
